64663d50034bedfeda9c8b5bf3b52e94c6351e66
Mustafa <mustafaalimutlu@gmail.com>
2023-01-25 20:54:03 +0300

    Implement UsageDetailsProcessorInterface to return dummy data. Reason: since emulator internet usage is low or even nonexistent, this eases developing how plots look.

---
+++ app/src/main/java/com/example/networkusage/usage_details_processor/UsageDetailsProcessorWithTestData.kt
@@ -0,0 +1,83 @@
+package com.example.networkusage.usage_details_processor
+
+import android.content.pm.PackageInfo
+import android.content.pm.PackageManager
+import java.time.ZonedDateTime
+import kotlin.random.Random
+
+class UsageDetailsProcessorWithTestData(
+    private val packageManager: PackageManager,
+) : UsageDetailsProcessorInterface {
+
+    private val packages: List<PackageInfo> =
+        packageManager.getInstalledPackages(PackageManager.GET_META_DATA)
+
+    override fun getUsageByUIDGroupedByTime(
+        uid: Int,
+        timeFrame: Pair<ZonedDateTime, ZonedDateTime>
+    ): List<GeneralUsageInfo> {
+
+        val random = Random(1)
+        var next2HourIntervalStart = timeFrame.first
+        val r = mutableListOf<GeneralUsageInfo>()
+        while (next2HourIntervalStart.isBefore(timeFrame.second)) {
+            r.add(
+                GeneralUsageInfo(
+                    random.nextLong(512) * 1024,
+                    random.nextLong(512) * 1024,
+                    next2HourIntervalStart.toInstant().toEpochMilli(),
+                    next2HourIntervalStart.plusHours(2).toInstant().toEpochMilli()
+                )
+            )
+            next2HourIntervalStart = next2HourIntervalStart.plusHours(2)
+
+        }
+        return r
+    }
+
+    override fun getUsageGroupedByTime(
+        timeFrame: Pair<ZonedDateTime, ZonedDateTime>,
+        networkType: NetworkType
+    ): List<GeneralUsageInfo> {
+        val random = Random(2)
+        var next2HourIntervalStart = timeFrame.first
+        val r = mutableListOf<GeneralUsageInfo>()
+        while (next2HourIntervalStart.isBefore(timeFrame.second)) {
+            r.add(
+                GeneralUsageInfo(
+                    random.nextLong(1024 ) * 1024,
+                    random.nextLong(1024 ) * 1024,
+                    next2HourIntervalStart.toInstant().toEpochMilli(),
+                    next2HourIntervalStart.plusHours(2).toInstant().toEpochMilli()
+                )
+            )
+            next2HourIntervalStart = next2HourIntervalStart.plusHours(2)
+
+        }
+        return r
+    }
+
+
+    override fun getUsageGroupedByUID(
+        timeFrame: Pair<ZonedDateTime, ZonedDateTime>,
+        networkType: NetworkType
+    ): List<AppUsageInfo> {
+        val random = Random(3)
+        val r = mutableListOf<AppUsageInfo>()
+        for (i in 0..10) {
+            val androidPackage = packages[random.nextInt(packages.size)]
+            r.add(
+                AppUsageInfo(
+                    androidPackage.applicationInfo.uid,
+                    androidPackage.packageName,
+                    androidPackage.packageName,
+                    random.nextLong(1024 * 1024) * 100,
+                    random.nextLong(1024 * 1024) * 10,
+                    packages[i].applicationInfo.loadIcon(packageManager)
+                )
+            )
+        }
+        return r
+    }
+
+}
